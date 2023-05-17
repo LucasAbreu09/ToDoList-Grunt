cf03112a8511bb5755f8378eae75ebdbfa685b38
Lucas Abreu <lucasabreu.c18@gmail.com>
2023-05-17 17:34:52 -0300

    "mudança na parte de conclusão da tarefa"

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,26 +2,62 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('submit', function addTable(e) {
         e.preventDefault();
 
+        //pegar valor no campo
         let text = document.getElementById('activite').value;
         let time = document.getElementById('time').value;
 
+        //inserir nova linha
         var table = document.getElementById('table');
         var newLine = table.insertRow();
 
+        //inserir texto/nome da atividade
         var newCell1 = newLine.insertCell();
         newCell1.innerHTML = text;
 
-        var newCell1 = newLine.insertCell();
-        newCell1.innerHTML = time + 'H';
+        //inserir a entreda do campo de tempo
+        var newCell2 = newLine.insertCell();
+        newCell2.innerHTML = time + 'H';
 
+        //adiciona div com label e checkbox
         var newCell3 = newLine.insertCell();
+
+        //colocar div
+        var checkboxDiv = document.createElement('div');
+        checkboxDiv.classList.add('checkbox');
+
         var checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
-        newCell3.appendChild(checkbox);
+        checkbox.id = 'concluido';
 
+        // inserir label e checkbox
+        var label = document.createElement('label');
+        label.innerHTML = 'Done';
+        label.classList.add('done');
+        label.setAttribute('for', 'concluido');
+
+        checkboxDiv.appendChild(checkbox);
+        checkboxDiv.appendChild(label);
+        newCell3.appendChild(checkboxDiv);
+
+        //limpar inputs
         document.getElementById('activite').value = '';
         document.getElementById('time').value = '';
-    }) 
+    });
+});
 
-    addTable();
-})
+//ler os novos campos inseridos e trocar as cores quando marcado ou não
+document.addEventListener('DOMContentLoaded', function() {
+    const table = document.getElementById('table');
+    table.addEventListener('change', function(event) {
+        const target = event.target;
+        if (target.matches('.checkbox input[type="checkbox"]')) {
+            const checkbox = target;
+            const label = checkbox.nextElementSibling;
+            if (checkbox.checked) {
+                label.style.backgroundColor = '#59C5B3';
+            } else {
+                label.style.backgroundColor = '#BF5846';
+            }
+        }
+    });
+});
